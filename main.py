459034ff4a4c9144224095972b7f1cf51e35d4c1
Jonas Bourguignon <jonas.bourguignon@hotmail.com>
2024-05-30 15:03:20 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from flask import Flask, render_template, request, redirect, session, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.secret_key = "ab34,89"
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///authentication.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
+
+
+@app.route("/")
+def home():
+    if "username" in session:
+        return redirect(url_for("dashboard"))
+    return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["passwor"]
+    user = User.query.filter_by(username = username).first()
+    if user and user.check_password(password):
+        pass
+    else:
+        return render_template("index.html")
+
+if __name__ in "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(25), unique=True, nullable=False)
+    password = db.Column(db.String(150), nullable=False)
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
